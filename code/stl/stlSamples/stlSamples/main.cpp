64d675163d976baaf9998ec69b2709de0cdc00e8
Milind Wanjari <milind.wanjari@gmail.com>
2016-09-16 22:50:16 +0800

    Added project for stl and a file for vector sample.

---
+++ code/stl/stlSamples/stlSamples/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  stlSamples
+//
+//  Created by Milind Wanjari on 16/9/16.
+//  Copyright © 2016 Milind Wanjari. All rights reserved.
+//
+
+#include <iostream>
+
+void VectorExamples();
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Welcome to the world of STL !!!\n";
+    
+    VectorExamples();
+    
+    return 0;
+}
---
+++ code/stl/stlSamples/stlSamples/stlVector.cpp
@@ -0,0 +1,18 @@
+//
+//  stlVector.cpp
+//  stlSamples
+//
+//  Created by Milind Wanjari on 16/9/16.
+//  Copyright © 2016 Milind Wanjari. All rights reserved.
+//
+
+#include <iostream>
+
+
+using namespace std;
+
+void VectorExamples()
+{
+    cout << "Running vector examples ..." << endl;
+    
+}
